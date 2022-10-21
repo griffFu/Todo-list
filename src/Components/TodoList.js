14258a9cb96110aae75405f0199170642152b940
GriffFu <gfulto.9@gmail.com>
2022-10-20 23:03:00 -0500

    added button to add items to list, time to change this into the updatd resume app lets goooo

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,6 +4,7 @@ import Todo from './Todo';
 
 function TodoList() {
    const [todos, setToDos] = useState([]);
+   const [wantForm, setWantForm] = useState(false);
 
    const addTodo = todo => {
        if(!todo.text || /^\s*$/.test(todo.text)){
@@ -14,12 +15,18 @@ function TodoList() {
 
        setToDos(newTodos)
        console.log(...todos);
+       setWantForm(!wantForm);
+   }
+   
+   const formToggle = () =>{
+       setWantForm(!wantForm)
    }
 
    return (
        <div>
            <h1>Whats the plan for today?</h1>
-           <ToDoForm onSubmit={addTodo}/>
+           <button onClick={formToggle}>add item</button>
+           {wantForm && <ToDoForm onSubmit={addTodo}/>}
            <Todo todos={todos}></Todo>
        </div>
    )
