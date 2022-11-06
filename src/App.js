1e6bfb938b6ddcf9af5ff933ecc3bba008c22716
GriffFu <gfulto.9@gmail.com>
2022-11-06 12:17:36 -0600

    adding gh pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Resume Builder</h1>
       </div>
       <div className="bg-white flex-grow flex justify-center align-center">
-        <div className="bg-white border border-solid border-black md:m-6  m-8 p-6 flex-1 flex-col w-12/12 md:w-6/12">
+        <div className="bg-white border border-solid border-black md:m-6  m-8 p-6 max-sm:flex-1 md:flex flex-col w-12/12 md:w-6/12">
           <PersonalInfo />
           <TodoList header = "Education"/>
           <TodoList header = "Skills"/>
